7d908672b96467affbd5de7071d4e1d204400a4e
ArtDiaNic <Artdianic@gmail.com>
2022-04-07 15:03:53 +0300

    rating was added

--- src/test/java/steps/BookingSteps.java
+++ src/test/java/steps/BookingSteps.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import staticdata.WebUrl;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,26 +33,35 @@ public class BookingSteps {
 
     @Given("User is on main screen")
     public void userIsOnMainScreen() {
-        driver.get("https://www.booking.com/searchresults.en-gb.html");
+        driver.get(WebUrl.BOOKING);
     }
 
     @When("User enters {string}")
     public void userEntersHotelName(String inputDestination) {
         String hotelName = "Hilton Hurghada Plaza Hotel";
-        String inputDestinationXpath = String.format("//input[@name='ss']",inputDestination);
+        String inputDestinationXpath = String.format("//input[@name='ss']", inputDestination);
         driver.findElement(By.xpath(inputDestinationXpath)).sendKeys(hotelName);
     }
-    @And("User clicks on the {string} button")
+
+    @And("User clicks on the Search button")
     public void userClicksOnTheButton() {
         String searchButtonXpath = "//button[@type ='submit']";
         driver.findElement(By.xpath(searchButtonXpath)).click();
     }
 
     @Then("The page shows {string} and its rating")
-    public void thePageShowsHotelNameAndItsRating() {
+    public void thePageShowsHotelNameAndItsRating(String hotelNameOnPage) {
         String hotelNameOnPageXpath = "//div[@data-testid = 'title' and contains(.,'Hilton Hurghada')]";
         String actualResult = driver.findElement(By.xpath(hotelNameOnPageXpath)).getText();
         String expectedResult = "Hilton Hurghada Plaza Hotel";
-        Assert.assertEquals(actualResult,expectedResult, "Hotel names are not the same");
+        Assert.assertEquals(actualResult, expectedResult, String.format("Hotel names are not the same", hotelNameOnPage));
+    }
+
+
+    @Then("The page shows {string} rating")
+    public void thePageShowsRating(String ratingOnPage) {
+        String ratingOnPageXpath = "//ancestor::div[@data-testid='property-card' and contains(.,'Hurghada Plaza')]//div[contains(@aria-label,'Scored')]";
+        String actualResult = driver.findElement(By.xpath(ratingOnPageXpath)).getText();
+        Assert.assertEquals(actualResult, ratingOnPage, String.format("The rating does not meet expectations", ratingOnPage));
     }
 }
